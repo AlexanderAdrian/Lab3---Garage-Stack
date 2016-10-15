eb1c4ed44fcf0228abf8df143f64f1cef6971361
Alexander Cardozo <alexander.a.cardozo@gmail.com>
2016-10-15 14:56:55 -0400

    Initial Commit

---
+++ Lab3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab3
+//
+//  Created by Alexander Adrian Cardozo on 10/15/16.
+//  Copyright © 2016 Alexander Adrian Cardozo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
